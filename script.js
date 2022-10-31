3f15d76f0c5c4be4986652332acd3f31d5e8858d
Tyler Bedard <tylerbedard126@gmail.com>
2022-10-31 13:11:20 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const APIkey = '871b3ff65e2c2af43749b9d344bcd5d5'
+
+let city;
+
+const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIkey;
+
+
+// fetch(queryURL)
